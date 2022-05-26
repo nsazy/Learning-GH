86104b50e67249df2d035734ad37223507e60f5c
Nick Sazy <nrsazy@yahoo.com>
2022-05-26 19:05:20 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Nicholas Sazy on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
